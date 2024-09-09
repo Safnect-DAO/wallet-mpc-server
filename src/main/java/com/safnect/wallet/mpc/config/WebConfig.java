b5932da7765fc12de7d8d71adcf576518d1a6b6f
shiw1962 <43884967+shiw1962@users.noreply.github.com>
2024-09-09 14:22:47 +0800

    Add files via upload

--- src/main/java/com/safnect/wallet/mpc/config/WebConfig.java
+++ src/main/java/com/safnect/wallet/mpc/config/WebConfig.java
@@ -1,12 +1,13 @@
 package com.safnect.wallet.mpc.config;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-import com.safnect.wallet.mpc.interceptor.AccessInterceptor;
+import com.safnect.wallet.mpc.web.AccessInterceptor;
 
 @Configuration
 @EnableWebMvc
@@ -15,10 +16,16 @@ public class WebConfig implements WebMvcConfigurer {
 	@Autowired
 	AccessInterceptor accessInterceptor;
 	
+	@Value("${app.prod}")
+	Boolean prod;
+	
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
-        registry.addInterceptor(accessInterceptor)
-                .addPathPatterns("/**"); // 拦截所有路径
-                //.excludePathPatterns("/excludePath"); // 排除特定路径
+    	if (this.prod) {
+            registry.addInterceptor(accessInterceptor)
+	            .addPathPatterns("/**") // 拦截所有路径
+	            .excludePathPatterns("/runes/rune-utxo/get") // 排除特定路径
+            	.excludePathPatterns("/fetch-data/abi-get"); // 排除特定路径
+    	}
     }
 }
