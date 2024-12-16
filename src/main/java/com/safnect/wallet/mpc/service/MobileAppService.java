abe7c6acb371adaf399132b7431e447ea1ed76b4
shiw1962 <43884967+shiw1962@users.noreply.github.com>
2024-12-16 10:06:05 +0800

    Create MobileAppService.java

---
+++ src/main/java/com/safnect/wallet/mpc/service/MobileAppService.java
@@ -0,0 +1,46 @@
+package com.safnect.wallet.mpc.service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.safnect.wallet.mpc.mapper.WalletAccountMapper;
+import com.safnect.wallet.mpc.mapper.WalletCardMapper;
+import com.safnect.wallet.mpc.mapper.WalletMapper;
+import com.safnect.wallet.mpc.model.Wallet;
+import com.safnect.wallet.mpc.model.WalletAccount;
+import com.safnect.wallet.mpc.model.WalletCard;
+
+@Service
+public class MobileAppService {
+
+	@Autowired
+	WalletMapper walletMapper;
+	
+	@Autowired
+	WalletAccountMapper walletAccountMapper;
+	
+	@Autowired
+	WalletCardMapper walletCardMapper;
+	
+	@Transactional
+	public void saveSignup(Wallet wallet, WalletAccount wa, List<WalletCard> wc) {
+		boolean exists = this.walletMapper.existsWithPrimaryKey(wallet.getWalletId());
+		if (!exists) {
+			this.walletMapper.insertSelective(wallet);
+			this.walletAccountMapper.insertSelective(wa);
+			this.walletCardMapper.insertList(wc);
+		}
+	}
+	
+	@Transactional
+	public void addAccount(WalletAccount wa) {
+		this.walletAccountMapper.insertSelective(wa);
+		Wallet wallet = new Wallet();
+		wallet.setWalletId(wa.getWalletId());
+		wallet.setAccountIndex(wa.getAccountIndex());
+		this.walletMapper.updateByPrimaryKeySelective(wallet);
+	}
+}
